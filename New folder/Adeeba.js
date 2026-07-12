bb323b76e40a1d2e3f9da773dc4313d8c7c21227
adeebaarif096 <adeebaarif096@gmail.com>
2026-07-12 09:14:20 +0500

    first commit

---
+++ New folder/Adeeba.js
@@ -0,0 +1,30 @@
+// let Addition
+// let p=100;
+// let d=50;
+// let z=p+d;
+// console.log(z);
+
+// let Substraction
+// let p=50;
+// let d=25;
+// let z=p-d;
+// console.log(z);
+
+// let multiplication
+// let p=30;
+// let d=30;
+// let z=d*p;
+// console.log(z);
+
+// let division
+// let p=60;
+// let d=2;
+// let z=p/d;
+// console.log(z);
+
+// let modulus 
+// let p=10;
+// let d=5;
+// let z=p%d;
+// console.log(z);
+
